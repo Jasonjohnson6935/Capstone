eb059351528d5b89df73dd556f2a2af55e856ebd
Jasonjohnson6935 <jason.johnson6935@gmail.com>
2024-03-03 13:26:23 -0600

    home page complete

---
+++ Ecommerce/src/components/Home.jsx
@@ -0,0 +1,11 @@
+import Home_bag from '../assets/Bag_home.jpg'
+function Home() {
+  return (<div>
+    <h1>Welcome to Jason's Store</h1>
+    <img src={Home_bag} />
+    <p>Shop until you drop</p>
+  </div>
+  )
+}
+
+export default Home;
